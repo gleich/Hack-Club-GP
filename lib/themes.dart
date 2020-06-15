acd58cc987be1823411895f6d05bdda33106726a
Matthew Gleich <matthewgleich@gmail.com>
2020-06-14 21:21:01 -0400

    🎨 Set primary color as const
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- lib/themes.dart
+++ lib/themes.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class Themes {
   // Shared Values
-  final primaryColor = Color.fromRGBO(236, 55, 80, 1);
+  static const _primaryColor = Color.fromRGBO(236, 55, 80, 1);
 
   ThemeData get darkTheme => ThemeData(
         brightness: Brightness.dark,
@@ -11,7 +11,7 @@ class Themes {
           color: Color.fromRGBO(35, 37, 40, 1),
         ),
         scaffoldBackgroundColor: Color.fromRGBO(23, 23, 29, 1),
-        primaryColor: primaryColor,
+        primaryColor: _primaryColor,
       );
 
   ThemeData get lightTheme => ThemeData(
@@ -20,6 +20,6 @@ class Themes {
           color: Color.fromRGBO(249, 250, 252, 1),
         ),
         scaffoldBackgroundColor: Colors.white,
-        primaryColor: primaryColor,
+        primaryColor: _primaryColor,
       );
 }
