04f7351b1b96725653ebb5b252d8a23c9e1e062a
Matthew Gleich <matthewgleich@gmail.com>
2020-06-17 20:35:33 -0400

    ✨ Add sick looking nav bar
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- lib/widgets/templates.dart
+++ lib/widgets/templates.dart
@@ -1,5 +1,6 @@
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 
 // 📦 Package imports:
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -20,10 +21,6 @@ class PageTemplate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bottomNavBarItems = <String>[
-      QRTradeRoute.name,
-    ];
-
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -39,27 +36,58 @@ class PageTemplate extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Theme.of(context).primaryColor,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(MdiIcons.qrcode),
-            title: Text(
-              'QR Trade',
-            ),
+      bottomNavigationBar: NavBar(),
+    );
+  }
+}
+
+class NavBar extends StatefulWidget {
+  @override
+  _NavBarState createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  final bottomNavBarItems = <String>[
+    QRTradeRoute.name,
+  ];
+  var _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SnakeNavigationBar(
+      style: SnakeBarStyle.floating,
+      snakeColor: Theme.of(context).primaryColor,
+      snakeShape: SnakeShape.rectangle,
+      selectedItemColor: Theme.of(context).scaffoldBackgroundColor,
+      backgroundColor: Theme.of(context).appBarTheme.color,
+      shadowColor: Colors.yellow,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(MdiIcons.qrcode),
+          title: Text(
+            'QR Trade',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(MdiIcons.nfcTap),
-            title: Text('NFC Trade'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(MdiIcons.scaleBalance),
-            title: Text('Balance'),
-          ),
-        ],
-        onTap: (int index) => Navigator.popAndPushNamed(
-          context,
-          bottomNavBarItems[index],
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(MdiIcons.nfcTap),
+          title: Text('NFC Trade'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(MdiIcons.scaleBalance),
+          title: Text('Balance'),
+        ),
+      ],
+      // shape:,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      currentIndex: _currentIndex,
+      onPositionChanged: (int index) => setState(
+        () => _currentIndex = index,
+      ),
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
         ),
       ),
     );
